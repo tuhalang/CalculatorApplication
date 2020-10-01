99104f494f29adec5de0cc8aa33b1d5e5eb35367
tuhalang <tuhalang007@outlook.com.vn>
2020-10-01 22:02:04 +0700

    add file

---
+++ app/src/main/java/com/tuhalang/calculator/Calcultor.java
@@ -0,0 +1,14 @@
+package com.tuhalang.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calcultor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcultor);
+    }
+}
